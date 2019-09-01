f53feb4ea022ec59ae979451e9d4db630e778f9f
Rodrigo Moroni <rodrigomoroni@hotmail.com>
2019-09-01 01:30:58 -0300

    adicionado outros plots, adicionado forma de carregamento parcial do csv

--- KDD BR/tarefa2.py
+++ KDD BR/tarefa2.py
@@ -1,35 +1,61 @@
 import pandas as pd
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 
 dataset_path = '/home/rodrigo/Documents/kddbr-2019/public/'
+#dataset_path = '/home/viviane/Documents/kddbr-2019/public/'
+#dataset_path = 'C:'
 
-def load_dataset():
+def load_dataset(skiprows, nrows):
     #abre o csv de treino e teste no caminho específicado
-    train = pd.read_csv(dataset_path + 'training_dataset.csv')
-    test = pd.read_csv(dataset_path + 'test_dataset.csv')
+    train = pd.read_csv(dataset_path + 'training_dataset.csv', skiprows=skiprows, nrows=nrows)
+    test = pd.read_csv(dataset_path + 'test_dataset.csv', skiprows=skiprows, nrows=nrows)
 
     #transforma em um único array e retorna
     full_data = [train, test]
     return full_data
 
+def data_map(data):
+    #Mapeia o valor de uma string para um int
+    cluster_map = {'X':0, 'Y':1, 'XY':2, 'L':3}
+    data['cluster'] = data['cluster'].map(cluster_map)
+    return data
+
 def plot(data):
     #Utiliza o signalX como eixo X e signalY como eixo Y
-    plt.plot(data['signalX'], data['signalY'], 'o', color='black');
+    plt.plot(data['signalX'], data['signalY'], 'o', color='black')
 
     #mostra na tela
     plt.show()
 
 def boxplot(data):
-    pass
+    plt.boxplot(data['signalX'])
+    plt.show()
+    plt.boxplot(data['signalY'])
+    plt.show()
 
 def hist_plot(data):
     pass
 
+def bar_plot(data):
+    plt.bar(data['signalX'], data['signalY'])
+    plt.show()
+
+def color_bar(data):
+    colors = ['red', 'green', 'blue', 'purple']
+    plt.scatter(data['signalX'], data['signalY'], c=data['cluster'], cmap=matplotlib.colors.ListedColormap(colors))
+    plt.show()
+
 if __name__ == "__main__":
 
+    #define parte do csv que será carregada
+    #dessa forma é possível fazer o carregamento e processamento por partes
+    #skiprows = 0 #Pula nenhuma linha
+    skiprows = range(1,385) #Ou seja ignora as linhas de 1 a 385 (preciso da linha 0 p/ colunas)
+    nrows = 384 #Quantas linhas serão carregadas
     #carrega o dataset
-    dataset = load_dataset()
+    dataset = load_dataset(skiprows, nrows)
 
     #no primeiro array, pega os elementos de 0 até 384
     sample = dataset[0][0:384] #[384:768] e assim por diante...
@@ -37,29 +63,21 @@ if __name__ == "__main__":
     #plota o gráfico
     plot(sample)
 
-    #conta quantos clusters X, Y e XY existem no dataset de treino (duas formas)
-    cluster_x = 0
-    cluster_y = 0
-    cluster_xy = 0
-    cluster_l = 0
+    bar_plot(sample)
 
-    cluster_x = len(dataset[0][dataset[0]['cluster'] == 'X'])
-    cluster_y = len(dataset[0][dataset[0]['cluster'] == 'Y'])
-    cluster_xy = len(dataset[0][dataset[0]['cluster'] == 'XY'])
-    cluster_l = len(dataset[0][dataset[0]['cluster'] == 'L'])
+    boxplot(sample)
 
-    '''for item in dataset[0]['cluster']:
-        if item == 'X':
-            cluster_x += 1
-        elif item == 'Y':
-            cluster_y += 1
-        elif item == 'XY':
-            cluster_xy += 1
-        else:
-            cluster_l += 1'''
+    sample = data_map(sample)
+    color_bar(sample)
+
+    #clusters X, Y e XY no dataset de treino
+    cluster_x = dataset[0][dataset[0]['cluster'] == 'X']
+    cluster_y = dataset[0][dataset[0]['cluster'] == 'Y']
+    cluster_xy = dataset[0][dataset[0]['cluster'] == 'XY']
+    cluster_l = dataset[0][dataset[0]['cluster'] == 'L']
 
     #imprime o resultados das contagens
-    print("Clusters\n",cluster_x,"são X\n",cluster_y,"são Y\n",cluster_xy,"são XY\n",cluster_l,"são L\n")
+    print("Clusters\n",len(cluster_x),"são X\n",len(cluster_y),"são Y\n",len(cluster_xy),"são XY\n",len(cluster_l),"são L\n")
 
     #imprime o menor valor da coluna silhouette
     print("Silhouette\nMenor valor ->",dataset[0]['silhouette'].min())
