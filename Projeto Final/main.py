7f17fe08ecaa2a7192608a777289d1464335c35b
Rodrigo Moroni <rodrigomoroni@hotmail.com>
2019-11-12 22:45:10 -0200

    gerando colunas: idade e dia da semana

--- Projeto Final/main.py
+++ Projeto Final/main.py
@@ -2,12 +2,12 @@ import pandas as pd
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LinearRegression
-from sklearn.cluster import KMeans
-from sklearn.metrics import mean_squared_error
-from sklearn.neural_network import MLPRegressor
-import skfuzzy as fuzz
+#from sklearn.preprocessing import StandardScaler
+#from sklearn.linear_model import LinearRegression
+#from sklearn.cluster import KMeans
+#from sklearn.metrics import mean_squared_error
+#from sklearn.neural_network import MLPRegressor
+#import skfuzzy as fuzz
 from sqlalchemy import create_engine
 from sklearn.model_selection import train_test_split
 from sklearn.base import BaseEstimator, TransformerMixin
@@ -18,6 +18,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier
 from sklearn.neighbors import KNeighborsClassifier
+from datetime import datetime
 
 dataset_path = '/home/rodrigo/Documents/dados_abertos_urbs/'
 #dataset_path = '/home/viviane/Documents/dados_abertos_urbs/'
@@ -56,19 +57,56 @@ class NumberSelector(BaseEstimator, TransformerMixin):
         return X[[self.key]]
 
 def load_dataset(skiprows=0, nrows=None):
-    entrada = pd.read_csv(dataset_path + 'entrada_cartao.csv', skiprows=skiprows, nrows=nrows, encoding='latin-1')
+    entrada = pd.read_csv(dataset_path + 'entrada_cartao.csv', skiprows=skiprows, nrows=nrows, encoding='latin-1', sep=';')
     cartao = pd.read_csv(dataset_path + 'cartao.csv', skiprows=skiprows, nrows=nrows)
     bairro = pd.read_csv(dataset_path + 'bairro.csv')
     full_data = [entrada, cartao, bairro]
     return full_data
 
+def data_treat(dataset):
+    # Tratamento das data de nascimento para idade
+    dataset[0]['DATANASCIMENTO'].fillna('01/01/20', inplace=True) # Isso vira 01/01/2020
+    # Gera uma lista contendo as idades ao invés da data de nsc
+    idade_list = [(2019 - datetime.strptime(dt_nsc, '%d/%m/%y').year) for dt_nsc in dataset[0]['DATANASCIMENTO'].tolist()]    
+    # Cria nova coluna (idade) no DF
+    dataset[0]['IDADE'] = idade_list
+    # Ninguem tem -1 de idade
+    dataset[0]['IDADE'].replace(-1, None)
+
+    # Tratamento da data/hora de embarque para dias da semana ("Monday is 0 and Sunday is 6")
+    dia_semana_embarque_list = [(datetime.strptime(dt_emb, '%Y-%m-%d').weekday()) for dt_emb in dataset[0]['DATAUTILIZACAO']]
+    dataset[0]['DIASEMANA'] = dia_semana_embarque_list
+    return dataset
+
 def data_map(dataset):
     #cluster_map = {'X':0, 'Y':1, 'XY':2, 'L':3}
     #data['cluster'] = data['cluster'].map(cluster_map)
-    return data
+    return dataset
 
-def all_plots(dataset):
-    pass
+def show_plots(dataset):
+    entrada = dataset[0]
+    cartao = dataset[1]
+    bairros = dataset[2]
+
+    # Box Plot da renda média dos bairros
+    plt.title('Renda Média dos Bairros')
+    plt.boxplot(bairros['RENDAMEDIAMENSAL'])
+    plt.show()
+
+    # Pie das origens
+    plt.title('Bairros de Origem')
+    plt.pie(cartao['CODIGOBAIRROORIGEM'].value_counts(), autopct='%1.1f%%')
+    plt.show()
+
+    # Pie dos destino
+    plt.title('Bairros de Destino')
+    plt.pie(cartao['CODIGOBAIRRODESTINO'].value_counts(), autopct='%1.1f%%')
+    plt.show()
+
+    # Uso de acordo com sexo
+    plt.title('Utilização por sexo')
+    plt.pie(entrada['SEXO'].value_counts(), labels=['F', 'M'], autopct='%1.1f%%')
+    plt.show()
 
 def criaDadosCsv():
 
@@ -106,12 +144,24 @@ def calculaCorrelacao(var_1, var_2):
 if __name__ == "__main__":
     print('Carregando os dados...')
     dataset = load_dataset()
+    print('Tratamento dos dados...')
+    dataset = data_treat(dataset)
+    #print('Plotagem...')
+    #show_plots(dataset)
+    exit(0)
 
     # entradas = alimentaDataframe()
     # print(entradas.head())
 
     # lê o dataset
-    entradas = pd.read_csv('entradas.csv', encoding='utf-8')
+    entradas = pd.read_csv(dataset_path + 'entradas.csv', encoding='utf-8')
+    
+    plt.title('Regionais')
+    plt.pie(entradas['REGIONAL'].value_counts(), labels=['Matriz', 'Boa Vista', 'St Felicidade', 'CIC', 'Fazendinha', 'Pinheirinho', 'Cajuru', 'Boqueirao', 'Tatu', 'Portao', 'Bairro Novo'],autopct='%1.1f%%')
+    plt.show()
+
+    print(entradas['REGIONAL'].value_counts())
+
     # seta a coluna como index
     entradas.set_index('INDEX', inplace=True)
 
