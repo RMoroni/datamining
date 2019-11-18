f5c3c0df988c30c3162dde5623ba6a3f28d96674
Fernanda Gubert <nanda.gubert@gmail.com>
2019-11-18 11:19:02 -0300

    novas features, separacao dos dados e avaliacao

--- Projeto Final/main.py
+++ Projeto Final/main.py
@@ -65,17 +65,45 @@ def load_dataset(skiprows=0, nrows=None):
 
 def data_treat(dataset):
     # Tratamento das data de nascimento para idade
-    dataset[0]['DATANASCIMENTO'].fillna('01/01/20', inplace=True) # Isso vira 01/01/2020
+    dataset['DATANASCIMENTO'].fillna('01/01/20', inplace=True)  # Isso vira 01/01/2020
     # Gera uma lista contendo as idades ao invés da data de nsc
-    idade_list = [(2019 - datetime.strptime(dt_nsc, '%d/%m/%y').year) for dt_nsc in dataset[0]['DATANASCIMENTO'].tolist()]    
+    idade_list = [(2019 - datetime.strptime(dt_nsc, '%d/%m/%y').year) for dt_nsc in dataset['DATANASCIMENTO'].tolist()]
     # Cria nova coluna (idade) no DF
-    dataset[0]['IDADE'] = idade_list
+    dataset['IDADE'] = idade_list
     # Ninguem tem -1 de idade
-    dataset[0]['IDADE'].replace(-1, None)
+    dataset.loc[(dataset['IDADE'] == -1), 'IDADE'] = 38  # 38 é a idade média
+    dataset.loc[(dataset['IDADE'] >= -30) & (dataset['IDADE'] < -20), 'IDADE'] = 84
+    dataset.loc[(dataset['IDADE'] >= -40) & (dataset['IDADE'] < -30), 'IDADE'] = 74
+    dataset.loc[(dataset['IDADE'] >= -50) & (dataset['IDADE'] < -40), 'IDADE'] = 64
 
     # Tratamento da data/hora de embarque para dias da semana ("Monday is 0 and Sunday is 6")
     dia_semana_embarque_list = [(datetime.strptime(dt_emb, '%Y-%m-%d').weekday()) for dt_emb in dataset[0]['DATAUTILIZACAO']]
     dataset[0]['DIASEMANA'] = dia_semana_embarque_list
+
+    # Binarização da coluna Sexo
+    sexoBinarizado = []
+    for sexo in dataset['SEXO']:
+        if sexo == 'M':
+            sexoBinarizado.append(0)
+        else:
+            sexoBinarizado.append(1)
+    dataset['SEXOBINARIZADO'] = sexoBinarizado
+
+    # Tratamento da data e hora para timestamp
+    hora_list = [str(hora) for hora in dataset['HORAUTILIZACAO']]
+    timestamp_list = [int(time.mktime(datetime.strptime(dt + ' ' + hora_list[i], "%Y-%m-%d %H").timetuple())) for i, dt
+                      in enumerate(dataset['DATAUTILIZACAO'])]
+    dataset['TIMESTAMP'] = timestamp_list
+
+    # Binarização das features categóricas utilizadas na predição
+    label_encoder = LabelEncoder()
+    bairro_binarizado = label_encoder.fit_transform(entradas['BAIRRO'])
+    entradas['BAIRROBINARIZADO'] = bairro_binarizado
+    regional_binarizada = label_encoder.fit_transform(entradas['REGIONAL'])
+    entradas['REGIONALBINARIZADA'] = regional_binarizada
+    cod_linha_binarizado = label_encoder.fit_transform(entradas['CODLINHA'])
+    entradas['CODLINHABINARIZADO'] = cod_linha_binarizado
+
     return dataset
 
 def data_map(dataset):
@@ -135,12 +163,6 @@ def criaDadosCsv():
 
     return entradas
 
-def calculaCorrelacao(var_1, var_2):
-
-    print('Correlação de Pearson: ',var_1.corr(var_2))
-
-    print('Correlação de Spearman: ', var_1.corr(var_2, method="spearman"))
-
 if __name__ == "__main__":
     print('Carregando os dados...')
     dataset = load_dataset()
@@ -165,43 +187,56 @@ if __name__ == "__main__":
     # seta a coluna como index
     entradas.set_index('INDEX', inplace=True)
 
-    # binariza a classe de predição e as features categóricas para calcular as correlações
-    label_encoder = LabelEncoder()
-    regional_binarizada = label_encoder.fit_transform(entradas['REGIONAL'])
-    entradas['REGIONALBINARIZADA'] = regional_binarizada
-    bairro_binarizado = label_encoder.fit_transform(entradas['BAIRRO'])
-    entradas['BAIRROBINARIZADO'] = bairro_binarizado
-
-    # calculaCorrelacao(entradas['HORAUTILIZACAO'], entradas['REGIONALBINARIZADA'])
-    '''Correlação de Pearson:  -0.05839568006612773
-    Correlação de Spearman:  -0.05537727124768886'''
-
-    # calculaCorrelacao(entradas['CLASSE'], entradas['REGIONALBINARIZADA'])
-    '''Correlação de Pearson:  0.13401692823046568
-    Correlação de Spearman:  0.1516167437961979'''
-
-    # calculaCorrelacao(entradas['BAIRROBINARIZADO'], entradas['REGIONALBINARIZADA'])
-    '''Correlação de Pearson:  0.10576365731450334
-    Correlação de Spearman:  0.09956378855415446'''
+    # cria as features
+    entradas = data_treat(entradas)
 
     # recebe os nomes das features e da variável alvo
-    features = ['HORAUTILIZACAO', 'BAIRROBINARIZADO', 'CLASSE', 'SEXO']
-    numeric_features = ['HORAUTILIZACAO', 'CLASSE', 'BAIRROBINARIZADO']
-    target = 'REGIONAL'
+    features = ['DIASEMANA', 'HORAUTILIZACAO', 'CODLINHABINARIZADO', 'REGIONALBINARIZADA', 'CLASSE', 'SEXOBINARIZADO', 'IDADE', 'BAIRROBINARIZADO', 'TIMESTAMP']
+    target = 'REGIONALDESTINO'
 
     # separa os dados em um conjunto de treino e outro de teste
-    X_train, X_test, y_train, y_test = train_test_split(entradas[features], entradas[target], test_size=0.33,
-                                                        random_state=42)
+    #X_train, X_test, y_train, y_test = train_test_split(entradas[features], entradas[target], test_size=0.33, random_state=42)
+
+    # NÃO ENCONTREI UMA FORMA AUTOMÁTICA DE SEPARAR OS DADOS EM TREINO E TESTE ESTABELECENDO DIFERENTES PROPORÇÕES PARA CADA
+    # CONJUNTO DE CLASSES DE PREDIÇÃO, POR ISSO FIZ MANUAL MESMO
+    # filtra as entradas por regional de destino
+    bairro_novo = entradas[entradas['REGIONALDESTINO'] == "Bairro Novo"]
+    boa_vista = entradas[entradas['REGIONALDESTINO'] == "Boa Vista"]
+    cajuru = entradas[entradas['REGIONALDESTINO'] == "Cajuru"]
+    boqueirao = entradas[entradas['REGIONALDESTINO'] == "Boqueirao"]
+    cic = entradas[entradas['REGIONALDESTINO'] == "CIC"]
+    fazendinha_portao = entradas[entradas['REGIONALDESTINO'] == "Fazendinha/Portao"]
+    matriz = entradas[entradas['REGIONALDESTINO'] == "Matriz"]
+    pinheirinho = entradas[entradas['REGIONALDESTINO'] == "Pinheirinho"]
+    santa_felicidade = entradas[entradas['REGIONALDESTINO'] == "Santa Felicidade"]
+    tatuquara = entradas[entradas['REGIONALDESTINO'] == "Tatuquara"]
+
+    # separa os dados em um conjunto de treino e outro de teste com proporções diferentes para cada classe de predição
+    X_train_matriz, X_test_matriz, y_train_matriz, y_test_matriz = train_test_split(matriz[features], matriz[target], train_size=0.2, random_state=42)
+    X_train_santa, X_test_santa, y_train_santa, y_test_santa = train_test_split(santa_felicidade[features], santa_felicidade[target], train_size=0.3, random_state=42)
+    X_train_boa, X_test_boa, y_train_boa, y_test_boa = train_test_split(boa_vista[features], boa_vista[target], train_size=0.3, random_state=42)
+    X_train_cic, X_test_cic, y_train_cic, y_test_cic = train_test_split(cic[features], cic[target], train_size=0.3, random_state=42)
+    X_train_fazendinha, X_test_fazendinha, y_train_fazendinha, y_test_fazendinha = train_test_split(fazendinha_portao[features], fazendinha_portao[target], train_size=0.4, random_state=42)
+    X_train_pinheirinho, X_test_pinheirinho, y_train_pinheirinho, y_test_pinheirinho = train_test_split(pinheirinho[features], pinheirinho[target], train_size=0.5, random_state=42)
+    X_train_cajuru, X_test_cajuru, y_train_cajuru, y_test_cajuru = train_test_split(cajuru[features], cajuru[target], train_size=0.6, random_state=42)
+    X_train_boqueirao, X_test_boqueirao, y_train_boqueirao, y_test_boqueirao = train_test_split(boqueirao[features], boqueirao[target], train_size=0.6, random_state=42)
+    X_train_tatuquara, X_test_tatuquara, y_train_tatuquara, y_test_tatuquara = train_test_split(tatuquara[features], tatuquara[target], train_size=0.8, random_state=42)
+    X_train_bairro, X_test_bairro, y_train_bairro, y_test_bairro = train_test_split(bairro_novo[features], bairro_novo[target], train_size=0.8, random_state=42)
+
+    # junta os subconjuntos de treino e teste
+    X_train = pd.concat([X_train_matriz, X_train_santa, X_train_boa, X_train_cic, X_train_fazendinha, X_train_pinheirinho, X_train_cajuru, X_train_boqueirao, X_train_tatuquara, X_train_bairro])
+    X_test = pd.concat([X_test_matriz, X_test_santa, X_test_boa, X_test_cic, X_test_fazendinha, X_test_pinheirinho, X_test_cajuru, X_test_boqueirao, X_test_tatuquara, X_test_bairro])
+    y_train = pd.concat([y_train_matriz, y_train_santa, y_train_boa, y_train_cic, y_train_fazendinha, y_train_pinheirinho, y_train_cajuru, y_train_boqueirao, y_train_tatuquara, y_train_bairro])
+    y_test = pd.concat([y_test_matriz, y_test_santa, y_test_boa, y_test_cic, y_test_fazendinha, y_test_pinheirinho, y_test_cajuru, y_test_boqueirao, y_test_tatuquara, y_test_bairro])
 
     #########################PIPELINE#########################
 
     # cria mini pipelines, um para cada transformador
 
     # seleciona e binariza variável sexo
-    '''sexo = Pipeline([
-        ('selector', TextSelector(key='SEXO')),
-        ('one-hot encoder', OneHotEncoder())
-    ])'''
+    sexo = Pipeline([
+        ('selector', NumberSelector(key='SEXOBINARIZADO'))
+    ])
 
     # seleciona a variável bairro
     bairro = Pipeline([
@@ -218,26 +253,38 @@ if __name__ == "__main__":
         ('selector', NumberSelector(key='CLASSE'))
     ])
 
-    # junta os mini pipelines
-    '''feats = FeatureUnion([('sexo', sexo),
-                          ('bairro', bairro),
-                          ('hora', hora),
-                          ('classe', classe)])'''
+    dia_semana = Pipeline([
+        ('selector', NumberSelector(key='DIASEMANA'))
+    ])
 
-    feats = FeatureUnion([('bairro', bairro),
+    cod_linha = Pipeline([
+        ('selector', NumberSelector(key='CODLINHABINARIZADO'))
+    ])
+
+    regional = Pipeline([
+        ('selector', NumberSelector(key='REGIONALBINARIZADA'))
+    ])
+
+    idade = Pipeline([
+        ('selector', NumberSelector(key='IDADE'))
+    ])
+
+    timestamp = Pipeline([
+        ('selector', NumberSelector(key='TIMESTAMP'))
+    ])
+
+    # junta os mini pipelines
+    feats = FeatureUnion([('sexo', sexo),
                           ('hora', hora),
-                          ('classe', classe)])
+                          ('codLinha', cod_linha),
+                          ('bairro', bairro),
+                          ('idade', idade)])
 
     # aplica as transformações nos dados de treino
     feature_processing = Pipeline([('feats', feats)])
     feature_processing.fit_transform(X_train)
 
     # cria o pipeline com os transformadores e o classificador escolhido
-    '''pipeline = Pipeline([
-        ('features', feats),
-        ('classifier', DecisionTreeClassifier(max_depth=3, random_state=0))
-    ])'''
-
     pipeline = Pipeline([
         ('features', feats),
         ('classifier', RandomForestClassifier(random_state=42))
@@ -249,5 +296,21 @@ if __name__ == "__main__":
     # aplica os dados de teste no modelo
     preds = pipeline.predict(X_test)
 
-    # valida o modelo (média de acertos)
-    print(np.mean(preds == y_test))
+    # imprime os labels para a matriz
+    print("Bairro Novo", "Boa Vista", "Cajuru", "Boqueirao", "CIC", "Fazendinha/Portao", "Matriz", "Pinheirinho", "Santa Felicidade", "Tatuquara")
+
+    # valida o modelo
+    # matriz de confusão
+    print(confusion_matrix(y_test, preds, labels=["Bairro Novo", "Boa Vista", "Cajuru", "Boqueirao", "CIC", "Fazendinha/Portao", "Matriz", "Pinheirinho", "Santa Felicidade", "Tatuquara"]))
+
+    # acurácia
+    print('Acurácia -> ', accuracy_score(y_test, preds))
+
+    # recall
+    print('Recall -> ', recall_score(y_test, preds, average='macro'))
+
+    '''
+    MELHOR ATÉ AGORA:
+    Acurácia ->  0.6316002238945208
+    Recall ->  0.6458925915711106
+    '''
